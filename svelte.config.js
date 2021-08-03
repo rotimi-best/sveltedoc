79132ae9ceb88ffbb4c9bbabf03502ba0801f673
rotimi-best <rotimiibitoyeemma@gmail.com>
2021-08-04 02:02:54 +0300

    Replace require with import

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 /** @type {import('@sveltejs/kit').Config} */
-const netlify = require('@sveltejs/adapter-netlify');
+import netlify from '@sveltejs/adapter-netlify';
+
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
