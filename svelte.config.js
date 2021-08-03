05137ac2b63747d7d9b0b890971816fea026b553
rotimi-best <rotimiibitoyeemma@gmail.com>
2021-08-04 01:55:37 +0300

    Netlify build steps

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
 /** @type {import('@sveltejs/kit').Config} */
+const netlify = require('@sveltejs/adapter-netlify');
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: netlify()
 	}
 };
 
