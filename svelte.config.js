7b4bef07dbcce1a1f5736dc7f0b61138179c09b1
rotimi-best <rotimiibitoyeemma@gmail.com>
2021-08-04 09:13:02 +0300

    Follow adapter-netlify docs

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 /** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-netlify';
 
 const config = {
 	kit: {
+		adapter: adapter(),
+
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: '@sveltejs/adapter-static'
+		target: '#svelte'
 	}
 };
 
