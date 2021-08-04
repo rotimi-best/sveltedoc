8dc7103502dddd6da2eefdefef6139b0bc8d7adb
rotimi-best <rotimiibitoyeemma@gmail.com>
2021-08-04 03:16:03 +0300

    Update netlify.toml

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
 /** @type {import('@sveltejs/kit').Config} */
+import netlify from '@sveltejs/adapter-netlify';
 
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: netlify()
 	}
 };
 
